43edef9d3be5cec2696a5f165e365fe1d13926d2
Vineeth Kumar Munigyala <munigyala@gmail.com>
2023-10-20 01:20:27 +0530

    create basic editor

---
+++ src/components/editor/Editor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './editor.css'
+const Editor = () => {
+  return (
+    <div className='editor'>
+        <textarea rows="15" cols="100" className='editor-textarea'></textarea>
+
+    </div>
+  )
+}
+
+export default Editor
